938791addfdbb4f95a0cab0c84c790a2173cbe4e
Incubator4 <aries0robin@gmail.com>
2017-07-10 23:24:26 +0800

    verion 0.8

--- Client/MainPage.py
+++ Client/MainPage.py
@@ -23,6 +23,25 @@ default_info = {
 }
 info_column=('id','name','sex','classes')
 
+
+
+
+
+def find_db(sql):
+    import pymssql
+    conn = pymssql.connect(
+        host='127.0.0.1',
+        user='Administator',
+        password='',
+        database='Wangchengzhang04Mis',
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute(sql)
+    result = cur.fetchall()
+    return result
+
+
+
 class MainPage(tk.Frame):
 
     def __init__(self, userinfo=default_info, master=None):
@@ -45,8 +64,20 @@ class MainPage(tk.Frame):
 
         self.TabStrip1__Tab1 = tk.Frame(self.TabStrip1)
 
+        self.subFrame = tk.Frame(self.TabStrip1__Tab1)
+        self.subFrame.pack(side=tk.TOP)
+
+        self.buttonwidth = 8
+
+        self.insertButton = tk.Button(self.subFrame, text='插入数据', font='微软雅黑', width=self.buttonwidth, command=lambda : change_db(mode='insert'))
+        self.insertButton.pack(anchor=tk.N, side=tk.LEFT, padx=10, pady=2)
+        self.updateButton = tk.Button(self.subFrame, text='更新数据', font='微软雅黑', width=self.buttonwidth, command=lambda : change_db(mode='update'))
+        self.updateButton.pack(anchor=tk.N, side=tk.LEFT, padx=10, pady=2)
+        self.deleteButton = tk.Button(self.subFrame, text='删除数据', font='微软雅黑', width=self.buttonwidth, command=lambda : change_db(mode='delete'))
+        self.deleteButton.pack(anchor=tk.N, side=tk.LEFT, padx=10, pady=2)
+
         self.scrollBar = tk.Scrollbar(self.TabStrip1__Tab1)
-        self.scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scrollBar.pack(side=tk.RIGHT, fill=tk.Y,expand=tk.YES)
 
         self.tree_1 = ttk.Treeview(self.TabStrip1__Tab1,
                                    columns=info_column,
@@ -70,7 +101,22 @@ class MainPage(tk.Frame):
         self.TabStrip1__Tab3 = tk.Frame(self.TabStrip1)
         self.TabStrip1__Tab3Lbl = tk.Label(self.TabStrip1__Tab3, text='Please add widgets in code.')
         self.TabStrip1__Tab3Lbl.place(relx=0.1, rely=0.5)
-        self.TabStrip1.add(self.TabStrip1__Tab3, text='查看成绩')
+        self.TabStrip1.add(self.TabStrip1__Tab3, text='插入数据')
+
+        def change_db(self,mode):
+            info = self.tree_1.get_children()
+            data = {
+                'mode': mode,
+                'info': info
+            }
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(address)
+            bytedata = repr(data).encode()
+            sock.send(bytedata)
+
+
+
+
 
 
 if __name__ == '__main__':
